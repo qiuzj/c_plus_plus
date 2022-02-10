bdd5348db44603a52c3dc21d59262aceba1f2160
qiuzj <zhanjia21@163.com>
2022-02-11 00:03:45 +0800

    pointer

---
+++ pointer.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int * p;
+    p = &a;
+    cout << "a的地址：" << &a << ", a的值：" << a << endl;
+    cout << "p：" << p << ", *p: " << *p << endl;
+
+    *p = 1000;
+    cout << "\na的地址：" << &a << ", a的值：" << a << endl;
+    cout << "p：" << p << ", *p: " << *p << endl;
+
+    cout << sizeof(p) << endl;
+    cout << sizeof(int *) << endl;
+    cout << sizeof(char *) << endl;
+    cout << sizeof(float *) << endl;
+    cout << sizeof(double *) << endl;
+    cout << sizeof(string *) << endl;
+    cout << sizeof(*p) << endl;
+
+    // 空指针
+    int * p1 = NULL;
+    // *p1 = 100; // error
+
+    // 野指针
+    int * p2 = (int *)0x1100;
+    cout << *p2 << endl;
+
+    return 0;
+}
---
+++ pointer_const.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+void bubble(int * arr, int len) {
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+    // 常量指针
+    const int * p1;
+    // *p1 = 11; // 错误
+    p1 = &b; // 正确
+
+    // 指针常量
+    int * const p2 = &a;
+    *p2 = 21; // 正确
+    // p2 = &b; // 错误
+
+    // const既修饰指针，也修饰常量
+    const int * const p3 = &a;
+    // *p3 = 30; // 错误
+    // p3 = &b; // 错误
+
+    int arr[10] = {1,10,3,5,2,9,6,4,7,8};
+    int * p = arr; // arr就是数组的首地址
+    for (int i = 0; i < 10; i++) {
+        // cout << arr[i] << endl;
+        cout << *p << endl;
+        p++;
+    }
+
+    int len = sizeof(arr) / sizeof(arr[0]);
+    // int * p4 = arr;
+    bubble(arr, len);
+    for (int i = 0, * p4 = arr; i < len; i++, p4++) {
+        cout << *p4 << endl;
+    }
+
+    return 0;
+}
